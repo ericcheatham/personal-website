a7a263516f6d594bae03b258782234d5b7253d2d
Eric Cheatham <ericjcheatham@gmail.com>
2019-11-16 15:21:20 -0600

    update package deps

--- src/components/Icons/ResumeIcon.tsx
+++ src/components/Icons/ResumeIcon.tsx
@@ -6,7 +6,8 @@ import { User } from 'grommet-icons'
 const Icon: IconType = ({ isFilled }) => (
     <NavbarIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         {isFilled ? (
-            <User fill="red"/>
+            <User 
+            fill="red"/>
         ) : ( 
             <User fill="red"/>
             )}
