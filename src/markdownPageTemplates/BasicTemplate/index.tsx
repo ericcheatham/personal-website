aabc728607d449594cc968edf191e2e9e6df217a
Eric Cheatham <ericjcheatham@gmail.com>
2019-11-16 15:22:39 -0600

    update package deps

--- src/markdownPageTemplates/BasicTemplate/index.tsx
+++ src/markdownPageTemplates/BasicTemplate/index.tsx
@@ -1,41 +1,38 @@
 import React from 'react'
-import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import Layout from '~/components/Layout'
 import PageWrapper from '~/components/PageWrapper'
 import MarkdownWrapper from '~/components/MarkdownWrapper'
 
-
 const BasicTemplate: React.FunctionComponent<{
-    data: any // type checked by GraphQL
-    location: {
-        pathname: string
-    }
+  data: any // type checked by GraphQL
+  location: {
+    pathname: string
+  }
 }> = ({
-    data: {
-        markdownRemark: {
-            frontmatter: { title },
-            html
-        }
-    },
-    location: { pathname }
+  data: {
+    markdownRemark: {
+      frontmatter: { title },
+      html
+    }
+  },
+  location: { pathname }
 }) => (
-            <Layout pathname={pathname}>
-                <PageWrapper>
-                    <div>
-                        <h1>{title}</h1>
-                        <MarkdownWrapper dangerouslySetInnerHTML={{ __html: html }} />
-                    </div>
-                </PageWrapper>
-            </Layout>
-        )
+  <Layout pathname={pathname}>
+    <PageWrapper>
+      <div>
+        <h1>{title}</h1>
+        <MarkdownWrapper dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </PageWrapper>
+  </Layout>
+)
 
 export default BasicTemplate
 
-
 export const BasicTemplateQuery = graphql`
-query BasicTemplate($slug: String!) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
+  query BasicTemplate($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       fields {
         slug
@@ -44,4 +41,5 @@ query BasicTemplate($slug: String!) {
         title
       }
     }
-  }`
+  }
+`
