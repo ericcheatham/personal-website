c227cce411bddd721eaf20c73d4763bd8cb86dcd
Eric Cheatham <ericjcheatham@gmail.com>
2020-01-27 12:02:06 -0600

    chore: replace icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/icon/me-icon.jpg`,
+        icon: `src/images/icon.jpg`,
       },
     },
     {
